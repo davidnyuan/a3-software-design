4250aa683fdbf103a10f6ea43c6def4ba21d402f
David Yuan <davidnyuan@gmail.com>
2017-05-11 12:03:12 -0700

    played around with repo

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 /* Create a scatter plot of 1960 life expectancy (gdp) versus 2013 life expectancy (life_expectancy).*/
 
-$(function() {
+$(function () {
     // Variables to show
     var xVar = 'gdp';
     var yVar = 'life_expectancy';
     var chartData,
-    nestedData;
+        nestedData;
 
     // Load data in using d3's csv function.
-    d3.csv('data/prepped_data.csv', function(error, data) {
-        var prepData = function() {
-            chartData = data.map(function(d) {
+    d3.csv('data/sampleData.csv', function (error, data) {
+        var prepData = function () {
+            chartData = data.map(function (d) {
                 return {
                     x: d[xVar],
                     y: d[yVar],
@@ -20,32 +20,34 @@ $(function() {
             });
 
         }
-        var scatter = VoronoiChart().width(700).height(400);
+        var scatter = VoronoiChart().width(1000).height(600).xTitle('Random x Title').yTitle('random y title');
 
         prepData();
 
-        var draw = function() {
+        var draw = function () {
             prepData();
-            scatter.title("Chart");
-            scatter.xTitle(xVar);
-            scatter.yTitle(yVar);
+            scatter.title('My title');
             var charts = d3.select('#vis').selectAll('.chart')
                 .data([chartData]);
-                charts.enter().append("div")
-                    .attr('class', 'chart')
-                    .merge(charts)
-                    .call(scatter);
-                charts.exit().remove();
+            charts.enter().append("div")
+                .attr('class', 'chart')
+                .merge(charts)
+                .call(scatter);
+            charts.exit().remove();
         };
 
         draw();
-        $('select').on('change', function(d) {
-        xVar = $(this).val();
-        draw();
-    });
+        $('#xAxisSelector').on('change', function (d) {
+            xVar = $(this).val();
+            draw();
+        });
 
-    // Initialize materialize style
-    $('select').material_select()
+        $('#radiusSelector').on('change', function (d) {
+            draw(scatter.radius($(this).val()));
+        });
+
+        // Initialize materialize style
+        $('select').material_select()
 
     });
 });
